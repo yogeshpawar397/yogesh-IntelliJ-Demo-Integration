90ba67b081e690c6c3138f881adc18c425db8e70
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-08-15 14:32:25 +0530

    Edit1(GitHub)

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
 
     public static void main(String[] args){
-        System.out.println("Hellow Yogesh...");
+        System.out.println("Hellow Yogesh...Edit1(GitHub)");
     }
 }
