08f36fa5ad87ae801e89afbdcda1563cfe62ad7b
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-08-15 15:23:50 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,8 @@
 public class Demo {
 
     public static void main(String[] args){
-        System.out.println("Hellow Yogesh...Edit1(GitHub)");
+
+        System.out.println("Hellow Yogesh...Edit1(GitHub)..." +
+                "Intergrated with GitHub desktop..");
     }
 }
