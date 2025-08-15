4a28de977a7fa9615b5ba45c5c5d300b34faad08
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-08-15 15:17:22 +0530

    IntelliJ 3.17

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,6 @@ public class Demo {
 
     public static void main(String[] args){
         System.out.println("Hellow Yogesh...Edit1(GitHub).." +
-                "IntelliJ1");
+                "IntelliJ1...");
     }
 }
