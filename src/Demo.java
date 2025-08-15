a7191911bbb48077ac71a430d24a704931331660
HP <yogeshpawar397@gmail.com>
2025-08-15 14:37:41 +0530

    IntelliJ 2.37

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
 
     public static void main(String[] args){
-        System.out.println("Hellow Yogesh...Edit1(GitHub)");
+        System.out.println("Hellow Yogesh...Edit1(GitHub).." +
+                "IntelliJ1");
     }
 }
