9a22cd3ceabf2f475700a16d2949aa9decccf1d0
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-09-02 08:50:14 +0530

    2 Sept 2025 8.45AM using intelliJ Termial

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
 
     public static void main(String[] args){
         System.out.println("Hellow Yogesh...Edit1(GitHub).." +
+                "2 Sept 2025" +
                 "IntelliJ1...");
     }
 }
