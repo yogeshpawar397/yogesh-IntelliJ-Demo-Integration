d0c8df72b4264f34256f5d95edc5e7040dc35edf
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-09-02 09:07:56 +0530

    added text as +3

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,6 @@ public class Demo {
     public static void main(String[] args){
 
         System.out.println("Hellow Yogesh...Edit1(GitHub)..." +
-                "Intergrated with GitHub desktop.. 2 Sept 2025 9.00 AM IntelliJ Terminal+ 2");
+                "Intergrated with GitHub desktop.. 2 Sept 2025 9.00 AM IntelliJ Terminal+ 2, +3");
     }
 }
