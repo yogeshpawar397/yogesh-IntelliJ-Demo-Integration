f3bbc61cf43a41128a920544f3beaab87063144d
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-09-03 22:06:30 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+  System.out.println("Test Hellow")
 }
