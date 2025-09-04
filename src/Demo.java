3c4e5f4c8238a9657e51fd2aa006c9feee0970ee
yogeshpawar397 <yogeshpawar397@gmail.com>
2025-09-04 08:31:24 +0530

    test1

--- src/Demo.java
+++ src/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
 
         System.out.println("Hellow Yogesh...Edit1(GitHub)..." +
                 "Intergrated with GitHub desktop.. 2 Sept 2025 9.00 AM IntelliJ Terminal+ 2, Modified in website, +3");
+
+        System.out.println("test1");
+
     }
 }
